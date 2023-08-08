521b85be289f43c8b67ae5cb3e270ddd0b2fe574
KevinAcz <kevinaceitonsalazar@gmail.com>
2023-08-08 09:37:59 -0400

    Added game function, and score tracking global variables

--- javascript.js
+++ javascript.js
@@ -19,30 +19,62 @@ function playRound (playerSelection, computerSelection) {
     let caseCheck = (playerSelection) => firstLetter + playerSelection.slice(1);
     playerSelection = caseCheck(playerSelection);
     //
+
     if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        computerScore++;
         return ('You Lose! Paper beats Rock');
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        playerScore++;
         return ('You Win! Paper beats Rock');
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        computerScore++;
         return ('You Lose! Scissors beat Paper');
     }
     else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        playerScore++;
         return ('You Win! Rock beat Scissors');
     }
     else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        return ('You Lose! Rock beat Scissors') 
+        computerScore++;
+        return ('You Lose! Rock beat Scissors'); 
     }
     else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        playerScore++;
         return ('You Win! Scissors beat Paper');
     }
     else {
+        tieScore++;
         return 'It\'s a tie!';
     }
 }
 
-let playerSelection = 'rock';
-
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+//DONE: Hacer que la funcion imprima 5 veces el ganador
+//DONE: HAcer que playround indique quien gana en un return
+//3 TODO: Hacer que game tenga una cuenta del puntaje y los muestre
+function game() {
+    let i = 1;
+    while (i <= 5) {
+        const playerSelection = prompt(`Round ${i}! What will you play`);
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        console.log(`Round number ${i}: ${result}`);
+        i++;
+    }
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+    if (playerScore > computerScore) {
+        console.log('You win! Congratulations!');
+    }
+    else if (playerScore < computerScore) {
+        console.log('You lost! Try again!');
+    }
+    else {
+        console.log(`Tie! You have tied ${tieScore} times!` );
+    }
+}
+let computerScore = 0;
+let playerScore = 0;
+let tieScore = 0;
+game();
